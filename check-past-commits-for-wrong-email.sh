9c0d1b2a2a1bad9e278396762f623bbd69b3deb1
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2017-07-17 11:58:46 +0200

    Force adding an email as a parameter

--- check-past-commits-for-wrong-email.sh
+++ check-past-commits-for-wrong-email.sh
@@ -2,6 +2,11 @@
 
 set -euf -o pipefail
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 forbidden_email"
+    exit 126 # 126 = Command invoked cannot execute
+fi
+
 set +e
 lines_with_wrong_email=$(git log --format="%aE %cE"|grep "$1"|wc -l)
 set -e
