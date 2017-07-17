8eaa8c076d1e9ba410f18b48001104f20ea26730
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2017-07-17 11:55:42 +0200

    Create check-past-commits-for-wrong-email.sh

---
+++ check-past-commits-for-wrong-email.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+set +e
+lines_with_wrong_email=$(git log --format="%aE %cE"|grep "$1"|wc -l)
+set -e
+
+if [ $lines_with_wrong_email -gt 0 ];then
+    echo "there are [past] commits with wrong email"
+    exit 1
+else
+    echo "no [past] commits with wrong email";
+    exit 0
+fi
